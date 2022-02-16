35ca8c24447166eed84792c3bb8cd013f8beeefb
amitkhajuria <amitkhajuria@healthpointe.com>
2022-02-16 14:17:24 +0530

    Section 5 Changes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, JsonpClientBackend } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './models/user';
+import { AccountService } from './services/account.service';
 
 //decorator
 @Component({
@@ -11,21 +13,31 @@ export class AppComponent implements OnInit {
   title = 'The Dating App';
   users :any;
 
-  constructor(private http:HttpClient){
+  constructor(
+    // private http:HttpClient,
+    private accountService: AccountService){
 
   }
 
   ngOnInit():void{
-    this.getUsers();
+    //this.getUsers();
+    this.setCurrentUser();
+  }
+  
+  setCurrentUser()
+  {
+    const user:User =JSON.parse(localStorage.getItem('user'));
   }
 
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-                this.users=response;
-              },error => {
-        console.log(error);
-    })
+  // getUsers(){
+  //   this.http.get('https://localhost:5001/api/users').subscribe(response => {
+  //               this.users=response;
+  //             },error => {
+  //               console.log(error);
+  //           },() => {
+  //               console.log('completed');
+  //   })
     //throw new Error("Method not implememted")
-  }
+  //}
  
 }
