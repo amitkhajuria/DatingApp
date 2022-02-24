a7dcc4fabe0ce816eef265c0657ab3de0dd2ded0
amitkhajuria <amitkhajuria@healthpointe.com>
2022-02-24 14:00:47 +0530

    Section 9 Changes2

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {  NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
 import { Member } from 'src/app/models/member';
 import { MembersService } from 'src/app/services/members.service';
 
@@ -11,12 +12,43 @@ import { MembersService } from 'src/app/services/members.service';
 export class MemberDetailComponent implements OnInit {
   
   member:Member;
+  
+  //for showing Photo 
+  galleryOptions:NgxGalleryOptions[];
+  galleryImages:NgxGalleryImage[];
 
   constructor(private memberService:MembersService,private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+
     this.loadMember();
+
+    this.galleryOptions=[
+      {
+      width:'500px',
+      height:'500px',
+      imagePercent: 100,
+      thumbnailsColumns:4,
+      imageAnimation:NgxGalleryAnimation.Slide,
+      preview:false
+      }
+    ]
+    //this.galleryImages=this.getImages();
+   
   }
+  getImages(): NgxGalleryImage[]{
+    const imageUrls=[];
+    for(const photo of this.member.photos) {
+      imageUrls.push({ 
+        small: photo?.url, 
+        medium: photo?.url,
+        big: photo?.url
+       });
+    }
+    console.log(imageUrls);
+    return imageUrls;
+  }
+
   loadMember()
   {
     console.log('loadMember called')
@@ -24,6 +56,10 @@ export class MemberDetailComponent implements OnInit {
     this.memberService.getMember(username).subscribe(data => {
       this.member=data;
       console.log(data);
+
+
+      //load the images once received data
+      this.galleryImages=this.getImages();
     })
   }
 
